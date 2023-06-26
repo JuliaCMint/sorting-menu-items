9d967946f7b281b8ce6c66f8fe468fb7c8f6e5eb
JuliaCMint <julia.malikova.jm@gmail.com>
2023-06-26 21:32:07 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+React sorting items
---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Title from "./Title";
+import menu from "./data";
+import { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+console.log(allCategories);
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;
